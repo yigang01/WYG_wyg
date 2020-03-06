eeb6c1734f560098c8c046f215290ce2da444847
WYG <1769703801@qq.com>
2020-03-06 23:32:09 +0800

    提交代码

---
+++ wygg1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<Windows.h>
+#include<math.h>
+#pragma warning(disable:4996)
+int main()
+{
+	int i = 0;
+	scanf("%d", &i);
+	printf("%d", i);
+    system("pause");
+	return 0;
+}
