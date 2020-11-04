2fbcda1f10ebfaff318869bcd3809def5e044253
WYGwyg <1769703801@qq.com>
2020-11-04 21:51:13 +0800

    练习模拟实现简单的空间配置器

---
+++ jjalloc.h
@@ -0,0 +1,85 @@
+#ifndef _JJALLOC_
+#define _JJALLOC_
+
+#include<new>
+#include<cstddef>
+#include<cstdlib>
+#include<climits>
+#include<iostream>
+namespace JJ
+{
+	template < class T > 
+	inline T* _allocate(ptrdiff_t size, T*)//开辟一段空间---new
+	{
+		set_new_handler(0);
+		T* tmp = (T*)(::operator new((size_t)(size * sizeof(T)));
+		if (tmp == 0)
+		{
+			cerr << "out of memory" << endl;
+			exit(1);
+		}
+		return tmp;
+	}
+	template<class T>
+	inline void _deallocate(T* buffer)//归还地址---delete
+	{
+		::operator delete(buffer);
+	}
+	template<class T1,class T2>
+	inline void _construct(T1* p, const T2& value)
+	{
+		new(p)T1(value);
+	}
+	template<class T>
+	inline void _destroy(T* ptr)
+	{
+		ptr->~T();
+	}
+	template<class T>
+	class allocator
+	{
+	public:
+		typedef T value_type;
+		typedef T* pointer;
+		typedef const T* const_pointer;
+		typedef T& reference;
+		typedef const T& const_reference;
+		typedef size_t size_type;
+		typedef ptrdiff_t difference_type;
+		
+		template<class U>
+		struct rebind
+		{
+			typedef allocator<U> other;
+		};
+		pointer allocate(size_type n, const void* hint = 0)//配置空间，返回空间的首地址
+		{
+			return _allocate((difference_type)n, (pointer)0);
+		}
+		void deallocate(pointer p, size_type n)//归还先前配置的空间
+		{
+			_deallocate(p);
+		}
+		void construct(pointer p, const T& value)
+		{
+			_construct(p, value);
+		}
+		void destroy(pointer p)//销毁这一段空间
+		{
+			_destroy(p);
+		}
+		pointer address(reference x)//返回某个对象的一个地址
+		{
+			return (pointer)&x;
+		}
+		const_pointer const_address(const_reference x)//返回某个const对象的地址
+		{
+			return (const_pointer)&x;
+		}
+		size_type max_size() const
+		{
+			return size_type(UINT_MAX / sizeof(T));
+		}
+	};
+}
+#endif
